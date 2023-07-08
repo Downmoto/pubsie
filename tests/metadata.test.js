a3022818d113dbe269499e934ad1c4f03ba7e6ba
Arad Fadaei <aradfadaie@gmail.com>
2023-07-08 18:22:04 -0400

    metadata test suite, error suite boilerplate

--- tests/metadata.test.js
+++ tests/metadata.test.js
@@ -5,18 +5,48 @@ const path = require("path");
 
 const epub_folder = path.join(__dirname, "./data/");
 
+function getRandomEpub() {
+  let files = fs
+    .readdirSync(epub_folder)
+    .filter((file) => file.endsWith(".epub"));
+
+  return files[~~(Math.random() * files.length)];
+}
+
 describe("metadata test suite", () => {
   it("should parse successfully", () => {
-    let files = fs
-      .readdirSync(epub_folder)
-      .filter((file) => file.endsWith(".epub"));
-
-    let file = files[~~(Math.random() * files.length)];
+    let file = getRandomEpub();
 
     let pub = new pubsie(epub_folder + file);
-    pub.parse()
+    pub.parse();
 
-    expect(pub.epub.mimetype).toBe('application/epub+zip')
-    expect(pub.epub.epubVersion).not.toBe(undefined)
+    expect(pub.epub.mimetype).toBe("application/epub+zip");
+    expect(pub.epub.epubVersion).not.toBe(undefined);
+  });
+
+  it("should parse metadata from root file(s)", () => {
+    let file = getRandomEpub();
+
+    let pub = new pubsie(epub_folder + file);
+    pub.parse();
+
+    let expected_object = {
+      identifier: expect.any(Array),
+      title: expect.objectContaining({
+        primary: expect.any(String),
+        additional: expect.any(Array),
+      }),
+      language: expect.objectContaining({
+        primary: expect.any(String),
+        additional: expect.any(Array),
+      }),
+      dcterms_modified: expect.any(String),
+    };
+
+    expect(pub.epub.metadata[0]).toMatchObject(expected_object);
   });
 });
+
+describe("metadata error test suite", () => {
+  it("should do nothing", () => {})
+})
