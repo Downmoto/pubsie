eee9d6e32e5bb62a060d17de719b7f044afeefc4
Arad Fadaei <aradfadaie@gmail.com>
2025-05-28 16:07:24 -0400

    solid progress

---
+++ pubsie.ts
@@ -0,0 +1,83 @@
+/**
+ * Pubsie package
+ * @author Arad Fadaei
+ * @see https://github.com/Downmoto/pubsie
+ *
+ * @license
+ * Copyright (c) 2025 Arad Fadaei.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import AdmZip from "adm-zip";
+import { parseString } from "xml2js";
+
+const MIMETYPE: string = "application/epub+zip";
+
+export class Pubsie {
+  #file: string;
+  #password: string | undefined;
+  #entries!: AdmZip.IZipEntry[];
+  #zip!: AdmZip;
+
+  constructor(pathToEpub: string, epubPassword?: string) {
+    this.#password = epubPassword;
+    this.#file = pathToEpub;
+    if (!this.#file) {
+      throw new Error("Pubsie requires file arg");
+    }
+
+    this.#openFile();
+    this.#validateMimetype();
+  }
+
+  #findEntry(entryName: string): AdmZip.IZipEntry | undefined {
+    return this.#entries.find((e) => e.entryName === entryName);
+  }
+
+  #extractContent(entry: AdmZip.IZipEntry): Buffer {
+    const result = this.#password
+      ? this.#zip.readFile(entry, this.#password)
+      : this.#zip.readFile(entry);
+
+    if (!result)
+      throw new Error(
+        "Failed to extract content: entry not found or unreadable"
+      );
+
+    return result;
+  }
+  #validateMimetype() {
+    const mimetypeFile = this.#findEntry("mimetype");
+
+    if (!mimetypeFile) {
+      throw new Error("No mimetype file found in archive");
+    }
+
+    const mimetype = this.#extractContent(mimetypeFile);
+    if (mimetype.toString("utf-8") !== MIMETYPE) {
+      throw new Error("Invalid mimetype in EPUB archive");
+    }
+  }
+
+  #openFile() {
+    this.#zip = new AdmZip(this.#file);
+    this.#entries = this.#zip.getEntries();
+  }
+}
---
+++ tests/pubsie.test.ts
@@ -0,0 +1,53 @@
+import { Pubsie } from '../pubsie';
+import * as path from 'path';
+import * as fs from 'fs';
+
+const epubPath = path.join(__dirname, 'test_files', 'Persia.epub');
+// test_password.zip has a single entry called mimetype
+// the contents are the epub mimetype
+const zipWithPasswordPath = path.join(__dirname, 'test_files', 'test_password.zip');
+
+describe('Pubsie', () => {
+  it('should open a file with a valid mimetype', () => {
+    expect(() => new Pubsie(epubPath)).not.toThrow();
+  });
+
+  it('should throw if file argument is missing', () => {
+    // @ts-expect-error
+    expect(() => new Pubsie()).toThrow('Pubsie requires file arg');
+  });
+
+  it('should throw if mimetype file is missing', () => {
+    // Create a zip without mimetype for this test
+    const AdmZip = require('adm-zip');
+    const tmpZipPath = path.join(__dirname, 'test_files', 'no_mimetype.zip');
+    const zip = new AdmZip();
+    zip.addFile('somefile.txt', Buffer.from('test'));
+    zip.writeZip(tmpZipPath);
+
+    expect(() => new Pubsie(tmpZipPath)).toThrow('No mimetype file found in archive');
+
+    fs.unlinkSync(tmpZipPath);
+  });
+
+  it('should throw if mimetype is invalid', () => {
+    // Create a zip with wrong mimetype
+    const AdmZip = require('adm-zip');
+    const tmpZipPath = path.join(__dirname, 'test_files', 'bad_mimetype.zip');
+    const zip = new AdmZip();
+    zip.addFile('mimetype', Buffer.from('not-an-epub'));
+    zip.writeZip(tmpZipPath);
+
+    expect(() => new Pubsie(tmpZipPath)).toThrow();
+
+    fs.unlinkSync(tmpZipPath);
+  });
+
+  it('should open a password-protected zip if password is correct', () => {
+    expect(() => new Pubsie(zipWithPasswordPath, '12345')).not.toThrow();
+  });
+
+  it('should throw if password is incorrect for password-protected zip', () => {
+    expect(() => new Pubsie(zipWithPasswordPath, 'wrong-password')).toThrow();
+  });
+});
